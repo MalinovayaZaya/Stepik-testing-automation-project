1fbb3dd870a107976c2d6e3c1930fe81c7b5e2a1
Polina Malinina <polinamalinina@MacBook-Air-Polina.local>
2021-08-25 18:06:28 +0300

    4-2 6 step: add Page Object method: add main page, where we can check login link

---
+++ pages/base_page.py
@@ -0,0 +1,16 @@
+from selenium.common.exceptions import NoSuchElementException
+
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
